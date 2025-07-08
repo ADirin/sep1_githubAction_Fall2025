8132f2210d9f499c0c0f6c1b592fd81e49be244a
amirdi <amir.dirin@yahoo.com>
2025-07-08 21:57:31 +0300

    Third Commit

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloTest {
+
+    @Test
+    public void testSomething() {
+        assertTrue(5 > 1, "5 should be greater than 1");
+    }
+}
